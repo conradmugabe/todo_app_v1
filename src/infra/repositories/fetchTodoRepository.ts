dd83249f9a3a086d82ddfaf3049558b04f91af67
conrad-evans <conradmugabe@gmail.com>
2022-09-17 18:01:20 +0300

    added fetch repository

--- src/infra/repositories/fetchTodoRepository.ts
+++ src/infra/repositories/fetchTodoRepository.ts
@@ -5,31 +5,54 @@ import { TodoRepository } from '../../use_cases/interfaces/TodoRepository';
 export default class FetchTodoRepository implements TodoRepository {
   private baseUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-  async getTodoList(): Promise<Todo[]> {
+  async getTodoList(): Promise<TodoProps[]> {
     const response = await fetch(this.baseUrl);
     const data: TodoProps[] = await response.json();
-    const todos = data.map(
-      (todo) => new Todo(todo.userId, todo.title, todo.completed)
-    );
+    const todos = data.map((todo) => new Todo(todo).toObject());
     return todos;
   }
 
-  getTodoById(id: number): Promise<Todo> {
-    const todo = new Todo(1, 'todo 1', true);
-    return new Promise((resolve) => resolve(todo));
-  }
+  getTodoById = async (id: number): Promise<TodoProps> => {
+    const response = await fetch(this.baseUrl + id);
+    const data: TodoProps = await response.json();
+    const todo = new Todo(data).toObject();
+    return todo;
+  };
 
-  addTodo(props: TodoRepository.AddTodoRequest): Promise<Todo> {
+  addTodo = async (
+    props: TodoRepository.AddTodoRequest
+  ): Promise<TodoProps> => {
     const { userId, title } = props;
-    const todo = new Todo(userId, title);
-    return new Promise((resolve) => resolve(todo));
-  }
+    const response = await fetch(this.baseUrl, {
+      method: 'POST',
+      body: JSON.stringify({ userId, title }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data: TodoProps = await response.json();
+    const todo = new Todo(data).toObject();
+    return todo;
+  };
 
-  deleteTodo(id: number): Promise<void> {
-    return new Promise((resolve) => resolve());
-  }
+  deleteTodo = async (id: number): Promise<void> => {
+    const response = await fetch(this.baseUrl + id, {
+      method: 'DELETE',
+    });
+    const data: void = await response.json();
+    return data;
+  };
 
-  updateTodo(todo: Todo): Promise<Todo> {
-    return new Promise((resolve) => resolve(todo));
-  }
+  updateTodo = async (todo: Todo): Promise<TodoProps> => {
+    const response = await fetch(this.baseUrl + todo.id, {
+      method: 'PUT',
+      body: JSON.stringify({ ...todo }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data: TodoProps = await response.json();
+    const updatedTodo = new Todo(data).toObject();
+    return updatedTodo;
+  };
 }
