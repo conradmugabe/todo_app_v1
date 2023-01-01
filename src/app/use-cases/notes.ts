261b409959884c81ae9c10937f056bf654cdca41
conrad mugabe <conradmugabe@gmail.com>
2023-01-02 00:09:58 +0300

    feat: added more use cases to notes

--- src/app/use-cases/notes.ts
+++ src/app/use-cases/notes.ts
@@ -1,4 +1,4 @@
-import { NoteData } from '../core/dto/note.dto';
+import { EditNoteData, NoteData } from '../core/dto/note.dto';
 import { Note } from '../core/entities/note';
 import NotesDatabaseService from '../core/services/notesDatabase.service';
 
@@ -9,4 +9,16 @@ export default class NotesUseCases {
 		this.databaseService.addNote({ title, body });
 
 	getNotes = async (): Promise<Note[]> => this.databaseService.getAllNotes();
+
+	getNoteById = async (noteId: string): Promise<Note> => {
+		const note = await this.databaseService.getNoteById(noteId);
+		if (!note) throw new Error('Note Not Found');
+		return note;
+	};
+
+	editNote = async ({ title, body, id }: EditNoteData): Promise<Note> => {
+		const note = await this.databaseService.editNote({ id, title, body });
+		if (!note) throw new Error('Something went wrong. Could not update note');
+		return note;
+	};
 }
