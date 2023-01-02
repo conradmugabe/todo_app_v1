4038fea2c866f463a91cf3a2337d9085ed2dff2e
conrad mugabe <conradmugabe@gmail.com>
2023-01-02 10:14:06 +0300

    fix: error fixes on application testing && solved import issues with react and eslint

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../src/App';
+import {
+	UseCases,
+	UseCasesProvider,
+} from '../src/notes/context/UseCasesContext';
+import LocalStorageDatabaseService from '../src/app/services/localStorageDatabaseService';
+import NotesUseCases from '../src/app/use-cases/notes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const database = new LocalStorageDatabaseService();
+const notesUseCases = new NotesUseCases(database);
+const useCases: UseCases = { notesUseCases };
+const queryClient = new QueryClient();
 
 describe('App tests', () => {
-	it('Should should display input value after click', async () => {
-		// render(
-		// 	<BrowserRouter>
-		// 		<App />
-		// 	</BrowserRouter>
-		// );
+	it('Should render app without crashing', () => {
+		render(
+			<UseCasesProvider useCases={useCases}>
+				<QueryClientProvider client={queryClient}>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</QueryClientProvider>
+			</UseCasesProvider>
+		);
 	});
 });
