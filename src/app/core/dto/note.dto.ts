2647be76b1efcf0072bb9d8ffca1688347ba0506
conrad mugabe <conradmugabe@gmail.com>
2023-01-03 22:35:17 +0300

    feat: added edit tag dto

--- src/app/core/dto/note.dto.ts
+++ src/app/core/dto/note.dto.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const HasId = z.object({ id: z.string() });
+export const HasId = z.object({ id: z.string() });
 
 export const NoteData = z.object({ title: z.string(), body: z.string() });
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- intentionally naming the variable the same as the type
--- src/app/core/dto/tag.dto.ts
+++ src/app/core/dto/tag.dto.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
+import { HasId } from './note.dto';
 
 export const AddTagProps = z.object({ label: z.string() });
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- intentionally naming the variable the same as the type
 export type AddTagProps = z.infer<typeof AddTagProps>;
+
+export const EditTagPros = AddTagProps.merge(HasId);
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- intentionally naming the variable the same as the type
+export type EditTagPros = z.infer<typeof EditTagPros>;
