a684a06a29651e74b068bb223ffe98b7b67316b3
conrad mugabe <conradmugabe@gmail.com>
2023-01-03 22:36:31 +0300

    feat: added more methods to tags database service

--- src/app/core/services/tagsDatabase.service.ts
+++ src/app/core/services/tagsDatabase.service.ts
@@ -1,8 +1,12 @@
-import { AddTagProps } from '../dto/tag.dto';
+import { AddTagProps, EditTagPros } from '../dto/tag.dto';
 import { Tag } from '../entities/tag';
 
 export default abstract class TagsDatabaseService {
 	abstract addTag({ label }: AddTagProps): Promise<Tag>;
 
 	abstract getTags(): Promise<Tag[]>;
+
+	abstract editTag(data: EditTagPros): Promise<Tag | undefined>;
+
+	abstract deleteTag(tagId: string): Promise<Tag | undefined>;
 }
