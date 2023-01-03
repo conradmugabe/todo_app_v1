73c84ba38b077ebdf7611ac17b3ab2c2948dc26b
conrad mugabe <conradmugabe@gmail.com>
2023-01-03 22:38:22 +0300

    feat: added more use cases for tags

--- src/app/use-cases/tags.ts
+++ src/app/use-cases/tags.ts
@@ -1,4 +1,4 @@
-import { AddTagProps } from '../core/dto/tag.dto';
+import { AddTagProps, EditTagPros } from '../core/dto/tag.dto';
 import { Tag } from '../core/entities/tag';
 import TagsDatabaseService from '../core/services/tagsDatabase.service';
 
@@ -9,4 +9,15 @@ export default class TagsUseCases {
 		this.databaseService.addTag({ label });
 
 	getTags = async (): Promise<Tag[]> => this.databaseService.getTags();
+
+	editTag = async ({ id, label }: EditTagPros): Promise<Tag> => {
+		const tag = await this.databaseService.editTag({ id, label });
+		if (!tag) throw new Error('Tag Not Found');
+		return tag;
+	};
+
+	deleteTag = async (tagId: string): Promise<void> => {
+		const tag = await this.databaseService.deleteTag(tagId);
+		if (!tag) throw new Error('Could not delete. Something went wrong');
+	};
 }
